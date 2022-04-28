4bec974deff2113e06ef4cc82607a8fde9c686e4
diego-008 <diegoacetun00345@gmail.com>
2022-04-28 16:17:13 -0600

    agregando gh-pages

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -2,10 +2,10 @@ export default function Progress(props) {
   return (
     <div className="container animate__animated animate__backInLeft" style={{ marginTop: "50px" }}>
       <div className="row align-items-center align-items-center">
-        <div className="col-3 text-light d-flex justify-content-end">
+        <div className="col-sm-3 text-light d-flex justify-content-sm-end">
           <p className="h2">{props.title}</p>
         </div>
-        <div className="col-9 text-light d-flex justify-content-start mb-2">
+        <div className="col-sm-9 text-light d-flex justify-content-start mb-2">
           <div className="progress" style={{width: "100%"}}>
             <div
               className={`progress-bar bg-${props.color}`}
