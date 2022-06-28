2999ea774c024dc3858522dea1253d53c60ee9e7
diego-008 <diegoacetun00345@gmail.com>
2022-06-28 11:52:47 -0600

    modificando dev skills

--- src/pages/DevelopmentSkills.js
+++ src/pages/DevelopmentSkills.js
@@ -4,13 +4,13 @@ export default function ProgramSkills() {
     <div className="container mt-auto mb-5">
       <div style={{ marginTop: "130px" }}>
         <p className="text-light h1 text-center">Mis hablidades de Desarrollo</p>
-        <Progress width="75" title="Python" color="info" />
-        <Progress width="30" title="Java" color="warning" />
         <Progress width="80" title="Html" color="info" />
-        <Progress width="60" title="CSS" color="info" />
-        <Progress width="60" title="Rect" color="info" />
+        <Progress width="50" title="CSS" color="info" />
+        <Progress width="70" title="JavaScript" color="warning" />
+        <Progress width="70" title="Python" color="info" />
         <Progress width="80" title="Bootstrap" color="info" />
-        <Progress width="30" title="JavaScript" color="warning" />
+        <Progress width="60" title="Rect" color="info" />
+        <Progress width="30" title="Java" color="warning" />
       </div>
 
     </div>
