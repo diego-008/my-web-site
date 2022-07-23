aac2543b5868120cedac52cb3ade1fbe34e3fa9e
diego-008 <diegoacetun00345@gmail.com>
2022-07-22 18:02:46 -0600

    cambiando iconos

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,23 @@
 import { Link } from "react-router-dom";
 import "./css/navbar.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHouse } from "@fortawesome/free-solid-svg-icons";
+import { faPaperclip } from "@fortawesome/free-solid-svg-icons";
+import Logo from "../images/logo.png";
 export default function NavBar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark fixed-top custom-navbar bg-dark">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/my-web-site">
-          <FontAwesomeIcon icon={faHouse} size="2x" />
+          {/* <FontAwesomeIcon icon={faHouse} size="2x" style={{color: "red"}} /> */}
+          <div className="d-flex">
+            <FontAwesomeIcon icon={faPaperclip} size="2x" className="" style={{color:"red"}} />
+            <img
+              src={Logo}
+              alt="logo"
+              className="img-fluid mb-2"
+              style={{ width: "70px" }}
+            />
+          </div>
         </Link>
         <button
           className="navbar-toggler"
@@ -30,36 +40,38 @@ export default function NavBar() {
                 data-bs-target="#navbarNavAltMarkup"
                 aria-controls="navbarNavAltMarkup"
                 aria-expanded="false"
-                
-                
                 aria-label="Toggle navigation"
-              >PROYECTOS</button>
+              >
+                PROYECTOS
+              </button>
               <h1 className="d-none d-lg-block">PROYECTOS</h1>
             </Link>
             <Link className="nav-link active ms-lg-5" to="/skills">
-            <button
+              <button
                 className="navbar-toggler text-light"
                 type="button"
                 data-bs-toggle="collapse"
                 data-bs-target="#navbarNavAltMarkup"
                 aria-controls="navbarNavAltMarkup"
                 aria-expanded="false"
-            
                 aria-label="Toggle navigation"
-              >HABILIDADES</button>
+              >
+                HABILIDADES
+              </button>
               <h1 className="d-none d-lg-block">HABILIDADES</h1>
             </Link>
             <Link className="nav-link  active ms-lg-5" to="/programskills">
-            <button
+              <button
                 className="navbar-toggler text-light"
                 type="button"
                 data-bs-toggle="collapse"
                 data-bs-target="#navbarNavAltMarkup"
                 aria-controls="navbarNavAltMarkup"
                 aria-expanded="false"
-              
                 aria-label="Toggle navigation"
-              >DESARROLLO</button>
+              >
+                DESARROLLO
+              </button>
               <h1 className="d-none d-lg-block">DESARROLLO</h1>
             </Link>
           </div>
--- src/components/Progress.js
+++ src/components/Progress.js
@@ -5,12 +5,7 @@ export default function Progress({ icon, colorIcon, title, color, width }) {
     <div className="container" style={{ marginTop: "50px" }}>
       <div className="row align-items-center align-items-center">
         <div className="col-sm-3 text-light d-flex justify-content-sm-end mb-2">
-          {/* <img
-            src={props.img}
-            className="img-fluid mt-2"
-            style={{ width: "24px", height: "24px" }}
-            alt=""
-          /> */}
+          
           <FontAwesomeIcon
             icon={icon}
             size="2x"
